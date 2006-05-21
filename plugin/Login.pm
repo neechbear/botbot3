de2f133205f3f5a1b05cdb0c827cb003ad35e621
Nicola Worthington <nicolaw@tfb.net>
2006-05-21 22:42:48 +0000

    git-svn-id: file:///var/tmp/data/svnroot/botbot3@480 610ec7d1-1c12-4784-ad90-13d12571cf29

--- plugin/Login.pm
+++ plugin/Login.pm
@@ -7,11 +7,16 @@ our %CMDHELP = ();
 
 sub handle {
 	my ($self,$event) = @_;
+
 	$self->say(sprintf('%s%s %s',
 			($self->{heap}->{config}->{force_login} ? '*' : ''),
 			$self->{heap}->{config}->{username},
 			$self->{heap}->{config}->{password}
 		)) if $event->{msgtype} eq 'HELLO' && !$self->{heap}->{logged_in};
+
+	# Set the logged_in flag to true if we get a response that could only
+	# ever happen if we're in a logged in state
+	$self->{heap}->{logged_in} = 1 if $event->{msgtype} =~ /^DONE|COMMENT$/;
 }
 
 1;
