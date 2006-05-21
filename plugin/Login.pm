19bc5e50f5f65ff9ba9d7736d5a58364e4f2cd9e
Nicola Worthington <nicolaw@tfb.net>
2006-05-21 22:32:28 +0000

    Adding
    git-svn-id: file:///var/tmp/data/svnroot/botbot3@478 610ec7d1-1c12-4784-ad90-13d12571cf29

---
+++ plugin/Login.pm
@@ -0,0 +1,18 @@
+package plugin::Login;
+use base plugin;
+use strict;
+
+our $DESCRIPTION = 'Login to talker';
+our %CMDHELP = ();
+
+sub handle {
+	my ($self,$event) = @_;
+	$self->say(sprintf('%s%s %s',
+			($self->{heap}->{config}->{force_login} ? '*' : ''),
+			$self->{heap}->{config}->{username},
+			$self->{heap}->{config}->{password},
+		) if $event->{msgtype} eq 'HELLO' && !$self->{heap}->{logged_in};
+}
+
+1;
+
